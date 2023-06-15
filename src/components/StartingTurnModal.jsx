a212b87aff52f1e7d34c05e9fc9b8b3abc828a14
idcetim <javirofe96@gmail.com>
2023-06-15 18:56:31 +0200

    Add style for startingModal

--- src/components/StartingTurnModal.jsx
+++ src/components/StartingTurnModal.jsx
@@ -8,14 +8,14 @@ const StartingTurnModal = ({ setTurn }) => {
   }
 
   return (
-    <div id="starting-modal" className="modal">
-      <div className="modal-content">
+    <div className="modal" id="starting-modal">
+      <div className="modal-content-turn"  >
         <header>
-          <h2> 🥇 Choose Player to start</h2>
+          <h2> 🎮 Choose player to start</h2>
         </header>
         <section className="play-again-section">
-          <button onClick={() => chooseStartingTurn(1)}>Player 1</button>
-          <button onClick={() => chooseStartingTurn(2)}>Player 2</button>
+          <button onClick={() => chooseStartingTurn(1)} className="btn-player1">Player 1</button>
+          <button onClick={() => chooseStartingTurn(2)} className="btn-player2">Player 2</button>
         </section>
       </div>
     </div>
